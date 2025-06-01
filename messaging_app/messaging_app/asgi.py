c2df45a26934287b0a6da371de1c6868890274ba
cox101 <104098884+cox101@users.noreply.github.com>
2025-06-02 00:08:52 +0300

    commit

--- messaging_app/messaging_app/asgi.py
+++ messaging_app/messaging_app/asgi.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 
 from django.core.asgi import get_asgi_application
--- messaging_app/messaging_app/wsgi.py
+++ messaging_app/messaging_app/wsgi.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 
 from django.core.wsgi import get_wsgi_application
