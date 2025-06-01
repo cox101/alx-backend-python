f130524135bb2bf69124aeac5d2ffab37c15f617
cox101 <104098884+cox101@users.noreply.github.com>
2025-06-02 00:04:36 +0300

    commit

---
+++ messaging_app/chats/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Conversation, Message
+
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2')}
+        ),
+    )
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Conversation)
+admin.site.register(Message)
