49739613b115bb199665de51de546f6b18a2917b
cox101 <104098884+cox101@users.noreply.github.com>
2025-08-14 19:06:41 +0300

    update

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 from chats.models import User
+from .managers import UnreadMessagesManager
 
 class Message(models.Model):
+
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     edited = models.BooleanField(default=False)
-
+    read = models.BooleanField(default=False)
+    # a parent_message field (self-referential foreign key) to represent replies.
+    parent_message = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    
+    # conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    objects = models.Manager()  # Default manager
+    unread = UnreadMessagesManager()  # Custom manager for unread messages
+    # class Meta:
+    #     ordering = ['-sent_at']
+    #     indexes = [
+    #         models.Index(fields=['conversation', 'sent_at']),
+    #         models.Index(fields=['sender']),
+    #     ]
+    
     def __str__(self):
         return f"Message from {self.sender} in {self.conversation}"
 
@@ -20,8 +35,11 @@ class Notification(models.Model):
     def __str__(self):
         return f"Notification for {self.recipient} regarding message {self.message.message_id}"
 
+
 class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    edited_at = models.DateTimeField(auto_now=True)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='edited_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
